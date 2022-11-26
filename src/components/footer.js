1db31df2b435d2010f89a696ae4da408c322582e
Harun <ahmedharun46@gmail.com>
2022-11-26 17:22:35 -0600

    Footer

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer(){
+    return(
+        <div class="footer" id="footer">
+            <a href="https://github.com/ahmedharun46"> alt="Github" </a>
+            <a href="https://www.linkedin.com/in/harun-ahmed-42b13a165/"> alt="LinkedIn" </a>
+            <a href="https://stackoverflow.com/users/19174368/harun-ahmed"> alt="Stackoverflow" </a>
+        </div>
+    )
+}
+
+
+
+export default Footer;
